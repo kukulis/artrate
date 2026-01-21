ecd8bb1da9dc4c12258bd680cd0bb5388280e42f
giedrius <giedrius@inkodus.lt>
2026-01-21 10:40:32 +0200

    "Apie" informacija.

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -9,6 +9,26 @@ import {RouterLink} from "vue-router";
     <h3>
       <RouterLink to="/privacy" class="link">Privacy policy</RouterLink>
     </h3>
+
+    The purpose of this site is to rank articles, not only by giving like or dislike, but to give a rank in various aspects,
+    like :
+    <ul>
+      <li>Accuracy</li>
+      <li>Objectivity</li>
+      <li>Structure</li>
+      <li>Ethics</li>
+      <li>Logic</li>
+    </ul>
+
+    <p>There is a possibility to rank articles manually or by using AI tools.</p>
+
+    <p>The motivation behind using AI for ranking, is to avoid bias, because people have own world view, and we <u>expect</u> the AI will be more objective.
+      You can read more about this site purpose at <a target="_blank" href="http://gt.darbelis.eu/?p=74">http://gt.darbelis.eu/?p=74</a>
+    </p>
+
+    <p>In the current version there is a possibility to rank manually for a regular user; to use AI tools you need to have admin role,
+      which you can ask through email info@darbelis.eu. The regular user may create 5 articles of maximum 10000 character length in 24 hours.
+      After 24 hours he may create another 5 articles and so on.</p>
   </div>
 
 </template>
